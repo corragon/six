2f15d2f01256028f2aa0ee430e30330dc7c9792f
Thomas Y <corragon@gmail.com>
2017-03-07 22:08:22 -0600

    Change order of arguments

--- src/TaskModel.js
+++ src/TaskModel.js
@@ -2,10 +2,10 @@ import Utils from './utils';
 import {DateOnly} from './util/time';
 
 export default class TaskModel {
-	constructor(description, id = Utils.guid(), completed = null, dateAdded = DateOnly()) {
+	constructor(description, completed = null, dateAdded = DateOnly(), id = Utils.guid()) {
     this.description = description;
-    this.id = id;
     this.completed = completed;
     this.dateAdded = dateAdded
+    this.id = id;
   }
 }
