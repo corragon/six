cb8c30913f78497ce5c01d0a327b527106a6ee7e
Thomas Y <corragon@gmail.com>
2017-03-13 03:49:58 -0500

    Trim down Child for easier reference reading

--- src/components/Child.js
+++ src/components/Child.js
@@ -11,13 +11,10 @@ import {dispatch} from 'ringa';
 import {attach, depend, dependency, walkReactParents} from 'react-ringa';
 import SixController from '../global/SixController';
 
-const DAY_OF_WEEK = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
 export default class Child extends React.Component {
   constructor() {
     super();
 
-    this.testDispatchMethod  = this.testDispatchMethod.bind(this);
     this.displayMessage  = this.displayMessage.bind(this);
   }
 
@@ -29,40 +26,17 @@ export default class Child extends React.Component {
     })
   }
 
-  testDispatchMethod() {
-    if (this.state.bus) {
-      dispatch(SixController.TEST_EVENT, {someValue: 'This is from Child'}, this.state.bus);
-    }
-    else {
-      console.warn('Could not find a bus on any parent');
-      return;
-    }
-  }
-
   displayMessage() {
     dispatch(SixController.UPDATE_MESSAGE, {message: 'This is from Child'}, this.state.bus);
   }
 
   render() {
-    let text = '';
-    let busName = '';
-
-    walkReactParents(this, (parent) => text += (parent.constructor.name || parent.toString()) + '\n');
-    walkReactParents(this, (parent) => busName += parent.bus ? parent.constructor.name + ': ' + parent.bus.id : '');
-
     return (
       <View>
-        <Text style={styles.welcome}>{text}</Text>
-        <Text style={styles.welcome}>{busName}</Text>
-        <Button
-          onPress={this.testDispatchMethod}
-          title="Fire Event"
-          color="#841584"
-        />
         <Button
           onPress={this.displayMessage}
           title="Update Message"
-          color="#841584"
+          style={styles.button}
         />
       </View>
     );
@@ -74,5 +48,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 5,
+  },
+  button: {
+    color: '#841584'
   },
 });
