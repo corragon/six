92b00cb99eba771960e5f75f57007247e02d62da
Thomas Y <corragon@gmail.com>
2017-03-29 22:51:04 -0500

    Add stripped down List model from ringa example app

---
+++ src/List.js
@@ -0,0 +1,52 @@
+import {Model} from 'ringa';
+
+class List extends Model {
+  //-----------------------------------
+  // Constructor
+  //-----------------------------------
+  /**
+   * Build a List.
+   *
+   * @param name The name of this list. Not the same as the id! Names can exist more than once in Ringa. Ids cannot.
+   * @param values Optional POJO with values to populate into this List.
+   */
+  constructor(values) {
+    super(values);
+
+    this.addProperty('items', values || []);
+
+    this.addProperty('editing', false);
+  }
+
+  //-----------------------------------
+  // Properties
+  //-----------------------------------
+
+
+  //-----------------------------------
+  // Methods
+  //-----------------------------------
+
+  /**
+   * Removes an item by its index and then notifies 'items'.
+   *
+   * @param ix
+   */
+  removeItemByIx(ix) {
+    this.items.splice(ix, 1);
+    this.notify('items');
+  }
+
+}
+
+/**
+ * Convert a simple Javascript object into a list. Used when data is returned from the server.
+ *
+ * @param obj A POJO.
+ * @returns {List} The new List object, ready for injection into the view layer.
+ */
+List.deserialize = function(obj) {
+  return new List(obj);
+};
+
+export default List;
