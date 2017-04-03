9906cd8c4f53755049ee3d4dcbaf19f8b09bf840
Thomas Y <corragon@gmail.com>
2017-04-02 22:01:07 -0500

    Revert Day and Task models to basic objects These models will be stored in the repository and we will write  wrapper objects (that extend Ringa Model)  to hold these pojos

--- src/DayModel.js
+++ src/DayModel.js
@@ -1,20 +1,11 @@
-import { Model } from 'ringa';
 import Utils from './utils';
-import { DateOnly } from './util/time';
+import {DateOnly} from './util/time';
 import TaskModel from './TaskModel';
-import List from './List';
 
-export default class DayModel extends Model {
-	constructor(tasks, date = DateOnly(), id = Utils.guid()) {
-    super();
-
-    if (tasks instanceof Array) {
-      tasks = List.deserialize(tasks);
-      console.warn(typeof tasks);
-    }
-
-    this.addProperty('tasks', tasks);
-    this.addProperty('date', date);
-    this.addProperty('id', id);
+export default class DayModel {
+  constructor(tasks, date = DateOnly(), id = Utils.guid()) {
+    this.tasks = tasks;
+    this.date = date;
+    this.id = id;
   }
 }
--- src/TaskModel.js
+++ src/TaskModel.js
@@ -1,14 +1,11 @@
-import { Model } from 'ringa';
 import Utils from './utils';
 import {DateOnly} from './util/time';
 
-export default class TaskModel extends Model {
-	constructor(description, completed = false, dateAdded = DateOnly(), id = Utils.guid()) {
-    super();
-
-    this.addProperty('description', description);
-    this.addProperty('completed', completed);
-    this.addProperty('dateAdded', dateAdded);
-    this.addProperty('id', id);
+export default class TaskModel {
+  constructor(description, completed = null, dateAdded = DateOnly(), id = Utils.guid()) {
+    this.description = description;
+    this.completed = completed;
+    this.dateAdded = dateAdded
+    this.id = id;
   }
 }
